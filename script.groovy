1ad6099a7cfdeca0428a9f0d3cbad1e419a7999c
mvenkatgit <156108418+mvenkatgit@users.noreply.github.com>
2024-09-06 06:46:49 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildApp () {
+  echo 'Building the application'
+}
+
+def testApp () {
+  echo 'Building the application'
+}
+
+def DeployingApp () {
+  echo 'Building the application'
+}
+return this
